12b0adf8261a578bfa513abcedf47d1f5a091cdb
alex cullen <alexander.m.m.cullen@ey.com>
2019-06-19 15:13:41 -0700

    Adding High/Low to Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 
-const forecast = (longitude, latitude, callback) => {
+const forecast = (latitude, longitude, callback) => {
     let url = 'https://api.darksky.net/forecast/52494d3a742c0704c32f0ece5abcbe31/' + encodeURIComponent(longitude) +',' + encodeURIComponent(latitude)
 
     request({url, json : true}, (error, { body }) => {
@@ -12,7 +12,8 @@ const forecast = (longitude, latitude, callback) => {
         callback("Yep, nope")
     }
     else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability +
+             '% chance of rain. \nThe high for the day is: ' + body.daily.data[0].temperatureHigh + ' \nThe low for today is: ' + body.daily.data[0].temperatureLow)
         }
     
 })
